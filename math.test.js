c673f55ffe48d1a62cc1344f7c6bb6928b89035b
eloyestebann51 <eloymax51@gmail.com>
2026-06-04 16:06:30 +0200

    feat: añadir Jest y tests unitarios

---
+++ math.test.js
@@ -0,0 +1,9 @@
+const { suma, multiplica } = require('./math');
+
+test('suma 2 + 3 debe ser 5', () => {
+    expect(suma(2, 3)).toBe(5);
+});
+
+test('multiplica 4 x 3 debe ser 12', () => {
+    expect(multiplica(4, 3)).toBe(12);
+});
